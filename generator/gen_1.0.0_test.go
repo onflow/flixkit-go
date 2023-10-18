b4921387a704d5045ce6269fb29b9d2c0b5549aa
Tom Haile <bradford.haile@gmail.com>
2023-10-18 11:02:40 -0500

    checkpoint, adding generator module

---
+++ generator/gen_1.0.0_test.go
@@ -0,0 +1,55 @@
+package generator
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/hexops/autogold/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateCommentBlock(t *testing.T) {
+	generator := Generator1_0_0{}
+	assert := assert.New(t)
+
+	code := `
+	import "FungibleToken"
+	import "FlowToken"
+	
+	/**
+	@f_version 1.1.0
+	@lang en-US
+	
+	@message title: Transfer Tokens
+	@message description: Transfer tokens from one account to another
+	
+	@parameter title amount: Amount
+	@parameter title to: To
+	@parameter description amount: The amount of FLOW tokens to send
+	@parameter description to: The Flow account the tokens will go to
+	
+	@balance amount: FlowToken
+	*/
+	transaction(amount: UFix64, to: Address) {
+	  let vault: @FungibleToken.Vault
+	
+	  prepare(signer: AuthAccount) {
+		self.vault <- signer
+		  .borrow<&{FungibleToken.Provider}>(from: /storage/flowTokenVault)!
+		  .withdraw(amount: amount)
+	  }
+	
+	  execute {
+		getAccount(to)
+		  .getCapability(/public/flowTokenReceiver)!
+		  .borrow<&{FungibleToken.Receiver}>()!
+		  .deposit(from: <-self.vault)
+	  }
+	}
+`
+	template, err := generator.GenerateFromComments(code)
+	assert.NoError(err, "GenerateFromComments should not return an error")
+	prettyJSON, err := json.MarshalIndent(template, "", "    ")
+	assert.NoError(err, "marshal template to json should not return an error")
+	autogold.ExpectFile(t, string(prettyJSON))
+}

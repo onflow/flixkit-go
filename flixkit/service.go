d12a1505f904e02cdcf2d7bb0fa154e99921d5dd
BoiseITGuru <dev@boiseitguru.com>
2024-03-04 20:21:21 -0800

    add default file reader

--- flixkit/service.go
+++ flixkit/service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/onflow/flixkit-go/internal"
+	filereader "github.com/onflow/flixkit-go/internal/file-reader"
 	"github.com/onflow/flixkit-go/types"
 )
 
@@ -15,6 +16,10 @@ type FlixService struct {
 func NewFlixService(config *types.FlixServiceConfig) *FlixService {
 	if config.FlixServerUrl == "" {
 		config.FlixServerUrl = internal.DefaultFlixServerUrl
+	}
+
+	if config.FileReader == nil {
+		config.FileReader = filereader.GetDefaultFileReader()
 	}
 
 	return &FlixService{

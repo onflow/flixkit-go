9226b0f45d21257f9ff83572d7cb70b29aa07546
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2024-05-14 10:18:03 -0700

    Remove

--- internal/v1_1/generator.go
+++ internal/v1_1/generator.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/onflow/flow-go-sdk/crypto"
-	"github.com/onflow/flowkit/v2/accounts"
 	"strings"
 
 	"github.com/onflow/cadence/runtime/ast"
@@ -51,12 +49,6 @@ func NewTemplateGenerator(contractInfos ContractInfos, logger output.Logger, net
 		if err != nil {
 			return nil, fmt.Errorf("could not initialize flowkit state %w", err)
 		}
-
-		emulatorAccount, err := accounts.NewEmulatorAccount(loader, crypto.ECDSA_P256, crypto.SHA3_256, "")
-		if err != nil {
-			return nil, fmt.Errorf("could not create emulator account %w", err)
-		}
-		state.Accounts().AddOrUpdate(emulatorAccount)
 
 		client := flowkit.NewFlowkit(state, network, gw, logger)
 		clients = append(clients, client)
